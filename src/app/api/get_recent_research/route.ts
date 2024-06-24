34513fca028e93612afbc38042ae4468c6bc4e63
Defy <61808223+De-y@users.noreply.github.com>
2024-06-24 16:18:54 -0700

    Commit

--- src/app/api/get_recent_research/route.ts
+++ src/app/api/get_recent_research/route.ts
@@ -1,34 +1,20 @@
 import { NextResponse } from "next/server";
-import axios from 'axios';
 
 export async function GET(request: any) {
-    return NextResponse.json({
-        'notice': "This API is deprecated. Please leverage the client."
-    }, {
-        'status': 299,
-        'statusText': 'Deprecated API'
-    })
-    // let repo_names = []
-    // for (let i = 1; i < 3; i++) {
-    //     let c = (await axios.get(`https://api.github.com/users/de-y/repos?page=${i}`)).data
-    //     if (c.length != 0) {
-    //         for (let repo in c) {
-    //             let [description, url, name, id] = [c[repo]['description'], c[repo]['html_url'], c[repo]['name'], c[repo]['id']]
-    //             repo_names.push({
-    //                 'name': name,
-    //                 'url': url,
-    //                 'description': description || 'No description.',
-    //                 'id': id
-    //             })
-    //         }                        
-    //     }        
-    // }
-    // repo_names.sort((a, b) => {
-    //     return a.id - b.id
-    // })
-    // console.log('TIME')
-    // return NextResponse.json({
-    //     'recentProject': repo_names[(repo_names.length)-1],
-    //     'secondRecentProject': repo_names[(repo_names.length)-2]
-    // })
+    return NextResponse.json([
+        {
+            'title': "Demonstration",
+            'authors': "de-y",
+            'description': 'No description provided',
+            'field': "Programming",
+        }
+    ])
+    // return NextResponse.json([
+    //     {
+    //         'title': null,
+    //         'authors': null,
+    //         'description': null || 'No description provided',
+    //         'field': null,
+    //     }
+    // ])
 }
--- src/components/Research.tsx
+++ src/components/Research.tsx
@@ -1,11 +1,38 @@
 'use client'
 import '@/css/components/Research.css'
+import { useEffect, useState } from 'react'
 
 export function Research() {
+    let [loadingStatus, setLoadingStatus] = useState(true)
+    let [mostRecentPaper, setMostRecentPaper] = useState(null)
+
+    useEffect(() => {
+        fetch('/api/get_recent_research').then((r) => {r.json().then((d) => {
+            setMostRecentPaper(d[d.length - 1])
+            setLoadingStatus(false)
+        })})
+    }, [])
     return (
         <div className='research'>
             <h1>My recent research</h1>
-            <p>None published yet.</p>
+            <div className='research-group'>
+                {loadingStatus ? null : (
+                    <>
+                        <div className='research-project'>
+                            {/* @ts-ignore */}
+                            <h1>{mostRecentPaper['title']}</h1>
+                            {/* @ts-ignore */}
+                            <p>{mostRecentPaper['authors']}</p>
+                            {/* @ts-ignore */}
+                            <p>Fields: {mostRecentPaper['field']}</p>
+                            {/* @ts-ignore */}
+                            <p>{mostRecentPaper['description']}</p>
+                        </div>
+                        <div className='research-vert'>
+                        </div>    
+                    </>
+                )}
+            </div>
         </div>
     )
 }
