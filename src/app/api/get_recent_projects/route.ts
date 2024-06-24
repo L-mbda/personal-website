86f106bc5f22697a42ddcdbb1c256cab49c78840
Defy <61808223+De-y@users.noreply.github.com>
2024-06-24 13:55:24 -0700

    YAY for API!

---
+++ src/app/api/get_recent_projects/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import axios from 'axios';
+
+export async function GET(request: any) {
+    let repo_names = []
+    for (let i = 1; i < 3; i++) {
+        let c = (await axios.get(`https://api.github.com/users/de-y/repos?page=${i}`)).data
+        if (c.length != 0) {
+            for (let repo in c) {
+                let [description, url, name, id] = [c[repo]['description'], c[repo]['html_url'], c[repo]['name'], c[repo]['id']]
+                repo_names.push({
+                    'name': name,
+                    'url': url,
+                    'description': description || 'No description.',
+                    'id': id
+                })
+            }                        
+        }        
+    }
+    repo_names.sort((a, b) => {
+        return a.id - b.id
+    })
+
+    return NextResponse.json({
+        'message': 'Sent.',
+    })
+}
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,25 +1,48 @@
 'use client';
 import '@/css/components/projects.css'
+import { useState } from 'react';
 import { BsArrowRight } from 'react-icons/bs';
 
 export function Projects() {
+    let [loadingStatus, setLoadingStatus] = useState(true);
+    fetch('http://localhost:3000/api/get_recent_projects')
     return (
-        <div className='projects'>
-            <h1>My most recent projects among those in the gallery.</h1>
-            <div className='proj-grid'>
-                <a className="recent-proj">
-                    <h2>Project</h2>
-                    <p>Description</p>
-                </a>
-                <div className="proj-group-vert">
-                    <div className='second-proj'>
-                        <h2>Project</h2>
+        <>
+            {loadingStatus ? (
+                <div className='projects'>
+                    <h1>Loading recent projects...</h1>
+                    <div className='proj-grid'>
+                        <a className="recent-proj loading_proj">
+                        </a>
+                        <div className="proj-group-vert">
+                            <a className='second-proj loading_proj'>
+                            </a>
+                            <a className='visit-gallery' href='/projects'>
+                                <p>Visit the gallery <BsArrowRight className='arrow' /></p>
+                            </a>
+                        </div>
                     </div>
-                    <a className='visit-gallery' href='/projects'>
-                        <p>Visit the gallery <BsArrowRight className='arrow' /></p>
-                    </a>
                 </div>
-            </div>
-        </div>
+            ) : (
+                    <div className='projects'>
+                        <h1>My most recent projects among those in the gallery.</h1>
+                        <div className='proj-grid'>
+                            <a className="recent-proj">
+                                <h2>Project</h2>
+                                <p>Description</p>
+                            </a>
+                            <div className="proj-group-vert">
+                                <a className='second-proj'>
+                                    <h2>Project</h2>
+                                </a>
+                                <a className='visit-gallery' href='/projects'>
+                                    <p>Visit the gallery <BsArrowRight className='arrow' /></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
