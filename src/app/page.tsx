cc86a287b805507bbdd3ed550a2492585a8a32b3
Defy <61808223+De-y@users.noreply.github.com>
2024-06-24 10:57:02 -0700

    Clear 🟢

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import '@/css/index.css'
-import { BiLogoYoutube } from "react-icons/bi";
+import { BiLogoGithub, BiLogoYoutube } from "react-icons/bi";
 
 export default function Home() {
   let descriptor = ["Programmer", "Student", "Researcher", "Developer", "YouTuber", "Gamer"]
@@ -20,10 +20,28 @@ export default function Home() {
         <h1><span className="mono">Hi, I'm</span> <span className="playrightnz" id="pnz">de-y.</span><br /> I am a <span id="descriptor">person with many interests</span>.</h1>
         <div className='socials'>
           <a className='youtube' href='https://youtube.com/@de-y'><BiLogoYoutube /></a>
+          <a className='github' href='https://github.com/de-y'><BiLogoGithub /></a>
         </div>
       </div>
-      <div className='d'>
-        <p>EJEs</p>
+      <div className='about'>
+        <h1>About Me</h1>
+        <div className='separator'>
+          <div className='about-left'>
+            <p className='aboutabout'>I am a rising junior at <a href='https://sandyridge.caslv.org/'>CASLV Sandy Ridge Campus</a>, where I am the president of the Hack Club Chapter and the president of the Science Olympiad Club. My main interests are within programming, chemistry, research, and medicine, with me being involved in several projects and taking science courses at school.</p>
+            <h2>Stats</h2>
+            <ul className='stats'>
+              <li>Age: 16</li>
+              <li>Location: Las Vegas, Nevada</li>
+              <li>Interests: Programming, Chemistry, Medicine, Research</li>
+              <li>Favorite Language: Python</li>
+              <li>Favorite Color: Green 🟢</li>
+            </ul>
+          </div>
+          <div className='about-right'>
+            <img src='/images/detroitgmhq.jpg' alt='me' height={375.40849673202614379084967320261} width={500} />
+            <p>Photo of the GM Headquarters in Detroit, Michigan</p>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,26 @@
+
 import '@/css/components/Footer.css'
-import { BiLogoYoutube } from 'react-icons/bi'
+import { BiLogoGithub, BiLogoYoutube } from 'react-icons/bi'
 export function Footer() {
+    let splashes = ["Sleep or no sleep? That is the question.","What is the meaning of life? 42.","Go show some love to Hack Club! ❤️","Peter's Award: Drink Java? Yes. Code Java? No.","Is there a limit to these splashes?","The moon is mine! 🌕 - Vector","Gotta... Do... More... Research", `Get ready! ${(new Date().getFullYear()) + 1}, here we come!`, 'Join  network!', `Coral Moment, I guess?`, '2024 Mr. Adem incident']
+    let splash = splashes[Math.floor(Math.random() * splashes.length)];
     return (
         <footer className='footer'>
             <div className='mecorner'>
                 <div className='top'>
                     <h1 className='name'>de-y</h1>
-                    <p>[PLACE SOMETJHING HERE]</p>
+                    <p>{splash}</p>
                 </div>
                 <div className='socialbar'>
                     <a className='youtube' href='https://youtube.com/@de-y'><BiLogoYoutube /></a>
+                    <a className='github' href='https://github.com/de-y'><BiLogoGithub /></a>
                 </div>
             </div>
             <div className='links-section'>
                 <div className='link-column'>
                     <h2>Links</h2>
                     <a href='/'>Projects</a>
-                </div>
-                <div className='link-column'>
-                    <h2>Links</h2>
+                    <a href='/'>Github</a>
                 </div>
             </div>
         </footer>
